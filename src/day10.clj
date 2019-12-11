a5a524fb4740671b1fbbe8de8a57a90e053ccbb1
Patrick Pichler <mail@patrickpichler.dev>
2019-12-11 05:55:50 +0100

    Improve day10 solution

--- src/day10.clj
+++ src/day10.clj
@@ -16,46 +16,31 @@
        (map-indexed #(parse-line %1 %2))
        (apply concat)))
 
-(defn distance [[ax ay] [bx by]]
-  (Math/sqrt (+ (Math/pow (- ax bx) 2) (Math/pow (- ay by) 2))))
+(defn to-polar [[x1 y1] [x2 y2]]
+  (let [dx (- x2 x1)
+        dy (- y2 y1)
+        length (Math/sqrt (+ (* dx dx) (* dy dy)))
+        angle (Math/atan2 dy dx)
+        angleInDegrees (mod (+ (Math/toDegrees angle) 360) 360)]
+    [angleInDegrees length]))
 
-(defn equal-enought? [x1 x2]
-  (< (Math/abs (- x1 x2)) 0.00001))
-
-(defn is-point-on-line? [[a b] c]
-  (let [d1 (distance a c)
-        d2 (distance b c)
-        d3 (distance a b)]
-    (equal-enought? (+ d1 d2) d3)))
-
-(comment
-  (is-point-on-line? [[0 0] [0 4]] [0 2])
-  (is-point-on-line? [[0 0] [4 4]] [2 2])
-  (is-point-on-line? [[0 0] [1 1]] [2 2])
-  (is-point-on-line? [[4 4] [1 1]] [2 2]))
-
-(defn is-direct-sight? [astroids [a1 a2 :as line]]
+(defn get-number-of-distinct-polars-per-astroid [astroids]
   (->> astroids
-       (filter #(and (not= % a1) (not= % a2)))
-       (some (partial is-point-on-line? line))
-       not))
-
-(defn increase [x]
-  (if (nil? x) 1
-      (inc x)))
-
-(defn increase-astroid-visible-count [m [a1 a2]]
-  (-> m
-      (update a1 increase)
-      (update a2 increase)))
+       (map (fn [a]
+              (->> astroids
+                   (filter #(not= a %))
+                   (map (partial to-polar a))
+                   (map first)
+                   distinct
+                   count
+                   ((partial vector a)))))
+       (into {})))
 
 (defn part-1 []
-  (let [astroids (parse-input)]
-    (->> (combo/combinations astroids 2)
-         (filter (partial is-direct-sight? astroids))
-         (reduce increase-astroid-visible-count {})
-         (reduce (fn [[p c :as a] [p2 c2 :as a2]]
-                   (if (> c c2) a a2))))))
+  (let [astroids (parse-input)
+        polars-per-astroid (get-number-of-distinct-polars-per-astroid astroids)]
+    (->> polars-per-astroid
+         (reduce #(if (> (second %1) (second %2)) %1 %2)))))
 
 (def number-of-decimal-places 100000)
 
@@ -64,16 +49,18 @@
 
 (defn get-angle [[x1 y1] [x2 y2]]
   (round-angle (let [result (Math/toDegrees (Math/atan2 (- y2 y1)  (- x2 x1)))]
-     (if (< result 0) (+ 360 result) 
-       result))))
+                 (if (< result 0) (+ 360 result)
+                     result))))
 
 (comment
   (get-angle [2 2] [2 1])
   (get-angle [2 2] [3 2])
   (get-angle [2 2] [2 3])
   (get-angle [2 2] [1 2])
-  (get-angle [11 13] [11 12])
-  )
+  (get-angle [11 13] [11 12]))
+
+(defn distance [[ax ay] [bx by]]
+  (Math/sqrt (+ (Math/pow (- ax bx) 2) (Math/pow (- ay by) 2))))
 
 (defn collect-astroids-on-angles [base-astroid astroids]
   (->> astroids
@@ -86,8 +73,7 @@
        (into {})))
 
 (defn part-2 []
-  (let [
-        base-astroid [23 19]
+  (let [base-astroid [23 19]
         astroids (parse-input)
         astroids-by-angle (collect-astroids-on-angles base-astroid astroids)
         start-angle (* 270 number-of-decimal-places)
@@ -105,5 +91,5 @@
          ((fn [[[x y] _]] (+ (* 100 x) y))))))
 
 (defn -main [& args]
-  (println (part-1)
+  (println (part-1))
   (println (part-2)))
