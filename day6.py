29c68cdeff9db4ffb740a84cfdf147980a08376f
Patrick Pichler <git@patrickpichler.dev>
2021-12-06 19:00:31 +0100

    Implement day6 with a dynamic approach

--- day6.py
+++ day6.py
@@ -1,70 +1,42 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
-from dataclasses import dataclass
+from functools import cache
 
 
-@dataclass
-class Fish:
-    num_fish: int
-    days_left: int
-
-
-def parse_input() -> list[Fish]:
-    count: dict[int, int] = defaultdict(int)
-
+def parse_input() -> list[int]:
     with open("inputs/day6.txt") as f:
-        for n in f.readline().strip().split(","):
-            count[int(n)] += 1
-
-    return [Fish(num, days) for days, num in count.items()]
+        return [int(n) for n in f.readline().strip().split(",")]
 
 
-def merge_fish(fish: list[Fish]) -> list[Fish]:
-    map: dict[int, Fish] = {}
+@cache
+def calc_offsprings(days_left: int, days_total: int) -> int:
+    if days_total <= days_left:
+        return 0
 
-    for f in fish:
-        if f.days_left in map:
-            map[f.days_left].num_fish += f.num_fish
-        else:
-            map[f.days_left] = f
-
-    return [f for _, f in map.items()]
+    return (
+        1
+        + calc_offsprings(6, days_total - days_left - 1)
+        + calc_offsprings(8, days_total - days_left - 1)
+    )
 
 
-def calculate_fish(fish: list[Fish], days: int):
-    for d in range(0, days):
-        fish_to_add = 0
-
-        for f in fish:
-            if f.days_left == 0:
-                fish_to_add += f.num_fish
-                f.days_left = 6
-            else:
-                f.days_left -= 1
-
-        if fish_to_add > 0:
-            fish.append(Fish(fish_to_add, 8))
-
-        fish = merge_fish(fish)
-
+def calc_fish(fish: list[int], days: int) -> int:
     sum = 0
-    for f in fish:
-        sum += f.num_fish
 
-    print(sum)
+    for f in fish:
+        sum += calc_offsprings(f, days)
+
+    return sum
 
 
 def solve_part_1():
     fish = parse_input()
-
-    calculate_fish(fish, 80)
+    print(calc_fish(fish, 80))
 
 
 def solve_part_2():
     fish = parse_input()
-
-    calculate_fish(fish, 256)
+    print(calc_fish(fish, 256))
 
 
 solve_part_1()
