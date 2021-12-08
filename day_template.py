15db3a7ae910eea8e33e820b48aad072860bcaf3
Patrick Pichler <git@patrickpichler.dev>
2021-12-08 08:23:20 +0100

    Add template

---
+++ day_template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def solve_part_1():
+    with open("inputs/day8.txt") as f:
+        pass
+
+
+def solve_part_2():
+    with open("inputs/day8.txt") as f:
+        pass
+
+
+solve_part_1()
+solve_part_2()
